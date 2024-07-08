7ef9f60e6cc1ddd6c496370f02b534b78f086b06
Its-sunny69 <115334102+Its-sunny69@users.noreply.github.com>
2024-07-08 21:11:09 +0530

    first push

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <p>Create Spcae: </p>
+        <button> Create + </button>
+      </div>
+    </>
+  )
+}
+
+export default App
