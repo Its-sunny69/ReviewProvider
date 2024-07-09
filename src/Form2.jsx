4913773bd4350eae47c144fa431f1e4881fcff3b
Inzamam Shaikh <92295764+456535688765@users.noreply.github.com>
2024-07-09 11:09:44 +0530

    Form

---
+++ src/Form2.jsx
@@ -0,0 +1,12 @@
+let Form2 = () => {
+  return (
+    <>
+      Name: <input type="text" />
+      <div>
+        Your Text: <textarea name="" id="" rows={10} column={10}></textarea>
+      </div>
+    </>
+  );
+};
+
+export default Form2;
