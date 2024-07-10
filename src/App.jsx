7edae2b2d941a7226740b99bf6ffc2179305e909
Inzamam Shaikh <92295764+456535688765@users.noreply.github.com>
2024-07-10 21:08:40 +0530

    Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
-import { useState } from 'react'
-import './App.css'
-
+import { useState } from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import UserInput from "./UserInput";
+import Home from "./Home";
+import UserDashboard from "./UserDashboard";
+import Review from "./Review";
 function App() {
-
   return (
-    <>
-      <div>
-        <p>Create Spcae: </p>
-        <button> Create + </button>
-      </div>
-    </>
-  )
+    <div>
+      <BrowserRouter>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/Form" element={<UserInput />} />
+          <Route path="/user-dashboard" element={<UserDashboard />} />
+          <Route path="/review" element={<Review />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/Review.jsx
@@ -0,0 +1,11 @@
+import { useNavigate } from "react-router-dom";
+
+function Review() {
+  return (
+    <>
+      <p>This is Review</p>
+    </>
+  );
+}
+
+export default Review;
