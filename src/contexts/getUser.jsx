6e4944f18bd2f95397baeaf2b6224ef46cc97c2d
Its-sunny69 <115334102+Its-sunny69@users.noreply.github.com>
2024-07-24 23:55:43 +0530

    #

--- src/contexts/getUser.jsx
+++ src/contexts/getUser.jsx
@@ -1,7 +1,6 @@
 import React, { createContext, useContext, useEffect, useState } from 'react'
 import { doc, getDoc } from "firebase/firestore";
 import { auth, store } from "../Store/realtimeDB";
-import { getAuth, signInAnonymously, onAuthStateChanged } from "firebase/auth";
 
 
 const authContext = createContext()
@@ -11,52 +10,19 @@ const AuthProvider = ({ children }) => {
     const [id, setId] = useState(null)
     const [loading, setLoading] = useState(true)
 
-    // const fetchUser = async () => {
-    //     auth.onAuthStateChanged(async (user) => {
-    //         setId(user.uid)
-    //         const docRef = doc(store, "users", user.uid);
-    //         const docSnap = await getDoc(docRef);
-    //         if (docSnap.exists()) {
-    //             setUserData(docSnap.data());
-    //         } else {
-    //             console.log("User is not loggedin");
-    //         }
-    //         setLoading(false)
-    //     });
-    // };
-
     const fetchUser = async () => {
         auth.onAuthStateChanged(async (user) => {
-          if (user) {
-            setId(user.uid);
+            setId(user.uid)
             const docRef = doc(store, "users", user.uid);
             const docSnap = await getDoc(docRef);
             if (docSnap.exists()) {
-              setUserData(docSnap.data());
+                setUserData(docSnap.data());
             } else {
-              console.log("User is not loggedin");
+                console.log("User is not loggedin");
             }
-          } else {
-            auth.signInAnonymously()
-              .then(async (result) => {
-                const user = result.user;
-                setId(user.uid);
-                const docRef = doc(store, "users", user.uid);
-                const docSnap = await getDoc(docRef);
-                if (docSnap.exists()) {
-                  setUserData(docSnap.data());
-                } else {
-                  console.log("User is not loggedin");
-                }
-              })
-              .catch((e) => {
-                console.error("Error during anonymous sign-in", error);
-              });
-          }
-    
-          setLoading(false);
+            setLoading(false)
         });
-      };
+    };
 
     useEffect(() => {
         setTimeout(() => {
