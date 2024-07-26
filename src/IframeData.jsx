7c2aa8216208c50953d25985aebf7e78052ff4c0
Its-sunny69 <115334102+Its-sunny69@users.noreply.github.com>
2024-07-26 23:30:30 +0530

    iframe

--- src/IframeData.jsx
+++ src/IframeData.jsx
@@ -1,7 +1,14 @@
 import React from "react";
-const IframeData = ({ content }) => {
-  console.log(content);
-  return <div>This is content:{content}</div>;
+import { useIframeContent } from "./contexts/IframeContentContext";
+
+const IframeData = () => {
+  const { iframeContent } = useIframeContent();
+  console.log("This is content", iframeContent);
+
+  if (!iframeContent) {
+    return <div>No content available</div>;
+  }
+  return <>{iframeContent}</>;
 };
 
 export default IframeData;
